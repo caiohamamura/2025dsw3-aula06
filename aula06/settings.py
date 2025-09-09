f831f3aef9cb63586d30385c4dfffda5f60d8afc
usuario <usuario@teste.com>
2025-09-09 14:36:18 -0300

    mend

--- aula06/settings.py
+++ aula06/settings.py
@@ -125,12 +125,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS  = [
 ]
 
-STORAGES = {
-    # ...
-    "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
-    },
-}
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
